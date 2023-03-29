4d260d0308fe52bd925d0b91a2ca46e97beb17bb
Angusvencent <115913884+Angusvencent@users.noreply.github.com>
2023-03-29 20:52:29 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://cdn.staticaly.com/gh/Angusvencent/images-hosting@master/images/jianbing.webp',
+      name: 'D33',
+      description: 'More on <a href="https://33hasnoheart.love" target="_blank" >Web</a>',
     },
   }
 }
